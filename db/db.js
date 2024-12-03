0c72ec62138bc66a8315f0daed2bac98e7b56e70
Saami <uthpala2001@gmail.com>
2024-12-03 10:16:47 +0530

    db updated

---
+++ db/db.js
@@ -0,0 +1,7 @@
+export let cropList=[];
+export let staffList=[];
+export let fieldList=[];
+export let logList=[];
+export let vehicleList=[];
+export let equipmentList=[];
+export let userList=[];
